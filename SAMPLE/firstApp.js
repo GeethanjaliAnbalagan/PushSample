975c176c2593d265c75b6a841b680efa3c7095dc
Geethanjali Anbalagan <geethanjali30@gmail.com>
2022-10-19 18:15:49 +0530

    Create firstApp.js

---
+++ SAMPLE/firstApp.js
@@ -0,0 +1,9 @@
+var name12 = "geetha";
+console.log(typeof name12);
+var arr = [];
+arr.push("java3");
+console.log(arr);
+function getTime() {
+    return new Date().getTime();
+}
+console.log(getTime());
