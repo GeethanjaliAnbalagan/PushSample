73ff8ec74da85874264c75e111721efe36e21ecd
Geethanjali Anbalagan <geethanjali30@gmail.com>
2022-10-19 18:16:34 +0530

    Create primitivetypenum.ts

---
+++ SAMPLE/primitivetypenum.ts
@@ -0,0 +1,16 @@
+let first:number = 34.8;
+let sam:number= 55;
+console.log(first);
+
+//String
+let name1:string="Geetha";
+console.log(name1);
+
+//Boolean
+let bool:boolean = true;
+
+//void
+let un:void = undefined;
+un = null; //null
+//un = "gee";
+
