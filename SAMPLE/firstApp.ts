8b580909c4203b8ef793726f33c5bf5615dda087
Geethanjali Anbalagan <geethanjali30@gmail.com>
2022-10-19 18:15:53 +0530

    Create firstApp.ts

---
+++ SAMPLE/firstApp.ts
@@ -0,0 +1,12 @@
+let name12:string="geetha";
+console.log(typeof name12)
+
+const arr:string[] =[];
+arr.push("java3");
+console.log(arr)
+
+function getTime():number{
+    return new Date().getTime();
+}
+
+console.log( getTime());
